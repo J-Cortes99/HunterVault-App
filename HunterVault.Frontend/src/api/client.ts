72a863d91568d2217800e63cd08f04c077e87864
J-Cortes <jorgocorto@gmail.com>
2026-04-05 18:09:09 +0200

    Apuntar URL de frontend a la nube de Azure

--- HunterVault.Frontend/src/api/client.ts
+++ HunterVault.Frontend/src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://huntervault-api-dvarbrfud9g2c9b6.spaincentral-01.azurewebsites.net/api',
   headers: { 'Content-Type': 'application/json' },
 });
